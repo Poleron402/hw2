252d06f5b521ae052fd3b0c38a46565612a1bce1
Poleron402 <zhperon@gmail.com>
2024-06-24 23:14:14 -0700

    minor improvements

--- js/script.js
+++ js/script.js
@@ -11,15 +11,25 @@ const isFormValid= ()=>{
 }
 
 
-const displayQ4Choices = ()=>{
+const displayQ4Thru8Choices = ()=>{
     let q4ChoicesArray = ["Maine", "Rhode Island", "Maryland", "Delaware"]
     q4ChoicesArray = _.shuffle(q4ChoicesArray)
 
     for (let i=0; i<q4ChoicesArray.length; i++){
         document.querySelector("#q4Choices").innerHTML += ` <input type="radio" name="q4" id= "${q4ChoicesArray[i]}" value="${q4ChoicesArray[i]}"> <label for="${q4ChoicesArray[i]}"> ${q4ChoicesArray[i]}</label>`
     }
+
+    let q5ChoicesArr = [{"choice": "Alaska, Texas, California", "flag" : "ATC"},
+        {"choice":"New York, Texas, Washington", "flag": "NTW"},
+        {"choice":"California, Wyoming, Texas", "flag":"CWT"}]
+    q5ChoicesArr = _.shuffle(q5ChoicesArr)
+    q5ChoicesArr.forEach(item => {
+        document.getElementById("q5Choices").innerHTML += `
+        <input type="checkbox" id="${item.flag}">  <label for="${item.flag}"> ${item.choice} </label><br>
+        `
+    });
 }
-displayQ4Choices()
+displayQ4Thru8Choices()
 const answerFeedback = (index, correct) =>{
     if (correct){
         document.getElementById(`q${index}Feedback`).textContent = "Correct!"
@@ -51,8 +61,11 @@ const gradeQuiz = ()=>{
     document.getElementById("Jefferson").checked && document.getElementById("Roosevelt").checked 
     && !document.getElementById("Jackson").checked 
     && !document.getElementById("Franklin").checked ? answerFeedback(3, true) : answerFeedback(3, false)
+    // FOURTH QUESTION VALIDATION
     let q4Response = document.querySelector("input[name=q4]:checked").value
     q4Response == "Rhode Island"? answerFeedback(4, true) : answerFeedback(4, false)
+    //FIFTH QUESTION VALIDATION
+    document.getElementById("ATC").checked && !document.getElementById("NTW").checked && !document.getElementById("CWT").checked?answerFeedback(5, true):answerFeedback(5, false)
     document.getElementById("totalScore").textContent = `Total score: ${score} pts`
     document.getElementById("totalAttempts").textContent = `Total Attempts: ${++attempts}`
     localStorage.setItem("totalAttempts", attempts)
